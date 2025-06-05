1b2c0765e01c2e929be59bd476b2cae28b31c394
Wang-Group-Cug <Wang_Group_Cug@163.com>
2025-06-05 15:53:20 +0800

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import requests
+
+
+st.set_page_config(page_title="Home", layout="wide")
+st.title("Main Page 🏠")
+st.write("Welcome to the homepage! BETA VERSION")
+#st.title('**Home**')
+#st.subheader('Welcome to ...')
+
+st.info('Contact: wangyq@cug.edu.cn')
